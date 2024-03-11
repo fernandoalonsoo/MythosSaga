80e05b9e129b5c020ab405bf21908157998bf815
Guillalox <guillalox@gmail.com>
2024-03-11 16:21:35 +0100

    Menu

---
+++ MythosSaga/src/mythossaga/GameManager.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mythossaga;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author guill
+ */
+class GameManager {
+    public void run(){       
+        Sistema system = new Sistema(); 
+        
+        try {
+            system.menuInicio();
+        } catch (IOException ex) {
+            Logger.getLogger(GameManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
